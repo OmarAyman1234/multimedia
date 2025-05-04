c3610f33be4581c88e659eaaa2340f64d39b4bd0
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-04 22:26:50 +0300

    ArticleController

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -23,6 +23,44 @@ class ArticleController {
       return $result;
     }
   }
+
+  public static function getArticleTranslations($articleId) {
+    $db = new DBController;
+    $db->openConnection();
+
+    $query = "SELECT t.*, l.name FROM translations as t JOIN languages as l ON t.languageId = l.id WHERE articleId=$articleId";
+    $result = $db->select($query);
+
+    if($result === false) {
+      echo 'Error in query';
+      return [];
+    } 
+    else if(count($result) === 0) {
+      return [];
+    }
+    else {
+      return $result;
+    }
+  }
+
+  public static function getAvailableLanguages($articleId) {
+    $db = new DBController;
+    $db->openConnection();
+
+    $query = "SELECT l.* FROM translations as t JOIN languages as l ON t.languageId = l.id WHERE articleId=$articleId";
+    $result = $db->select($query);
+
+    if($result === false) {
+      echo 'Error in query';
+      return [];
+    } 
+    else if(count($result) === 0) {
+      return [];
+    }
+    else {
+      return $result;
+    }
+  }
 }
 
 
