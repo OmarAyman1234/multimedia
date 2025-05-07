c7e91f4b52e9d2300ffeffcb26e5a51637c63632
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-07 12:31:18 +0300

    New DBController usage example (Select should be in model, that's just an example)

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -6,11 +6,12 @@ require_once '../../controllers/DBController.php';
 class ArticleController {
 
   public static function getArticle($articleId) {
-    $db = new DBController;
-    $db->openConnection();
+    // $db = new DBController;
+    // $db->openConnection();
 
     $query = "SELECT a.*, rg.username AS editorUsername, cat.name AS categoryName FROM articles AS a JOIN registeredUsers AS rg ON a.editorId = rg.id JOIN categories AS cat ON cat.id = a.categoryId WHERE a.id=$articleId";
-    $result = $db->select($query);
+    // $result = $db->select($query);
+    $result = DBController::select($query);
 
     if($result === false) {
       echo 'Error in query';
