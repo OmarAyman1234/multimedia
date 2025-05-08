57ff63d01a3dd24b361c6a3307bd6518dab9b246
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-08 23:52:48 +0300

    list model update

--- models/list.php
+++ models/list.php
@@ -25,30 +25,30 @@ class Lists {
         return $this->userId;  
     }
 
-    public function editList(Lists $list ){
+    public function editList(Lists $list){
         $listId = $list->getListId();
         $listNewName = $list->getListName();
-        $query = "UPDATE `lists` SET `name`='$listNewName' WHERE id = '" . $listId . "'";
+        $query = "UPDATE `lists` SET `name`='$listNewName' WHERE id = $listId";
         $result = DBController::delete($query);
         return $result;
     }
 
-    public function getLists($userId){
-        $query = "SELECT * FROM lists WHERE isDeleted = 0 and userId='" . $userId . "'";
-        $result =  DBController::select($query);
+    public static function getLists($userId){
+        $query = "SELECT * FROM lists WHERE isDeleted = 0 and userId= $userId";
+        $result = DBController::select($query);
         return $result;
     }
 
     public function addList(Lists $list){
         $listName = $list->getListName();
         $userId = $list->getUserId();
-        $query = "INSERT INTO lists (name, userId) VALUES ('$listName', '$userId')"; 
+        $query = "INSERT INTO lists (name, userId) VALUES ('$listName', $userId)"; 
         $result = DBController::insert($query);
         return $result;
     }
     
     public function deleteList($listId){
-        $query = "UPDATE lists SET isDeleted = 1 WHERE id = '" . $listId . "'";
+        $query = "UPDATE lists SET isDeleted = 1 WHERE id = $listId";
         $result = DBController::delete($query);
         return $result;
     }
@@ -57,13 +57,13 @@ class Lists {
         $query = "SELECT a.id AS article_id, a.title, a.content, a.image 
         FROM articles a 
         JOIN lists_articles la ON a.id = la.articleId 
-        WHERE la.listId = " . $listId;
+        WHERE la.listId = $listId";
         $result = DBController::select($query);
         return $result;
     }
 
-    public  function removeArticleFromList( $listId,$articleId){
-        $query = "DELETE FROM lists_articles WHERE articleId = " . $articleId . " and listId = " . $listId;
+    public  function removeArticleFromList($listId, $articleId){
+        $query = "DELETE FROM lists_articles WHERE articleId = $articleId and listId = $listId";
         $result = DBController::delete($query);
         return $result;
     }
