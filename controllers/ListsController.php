1383dea451cb04f048a323970f8e59609459d6cd
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-08 23:52:56 +0300

    list controller update

--- controllers/ListsController.php
+++ controllers/ListsController.php
@@ -2,56 +2,78 @@
 require_once '../../models/client.php';
 require_once '../../models/list.php';
 require_once '../../controllers/DBController.php';
+require_once '../../controllers/AuthController.php';
 class ListsController{
     public static function getLists($userId){
-        $list = new Lists();
-        $result = $list->getLists($userId) ;
-        if($result === false) {
-            echo 'Error in query';
-            return;
+        if(AuthController::isLoggedIn() && $_SESSION['userId'] == $userId) {
+
+            $result = Lists::getLists($userId);
+    
+            if($result === false) {
+                echo 'Error in query';
+                return [];
+            } 
+            else if(count($result) === 0) {
+                return [];
+            }
+            else {
+                return $result;
+            }          
         } 
-        else if(count($result) === 0) {
-            header('location: ../views/Shared/404.php');
-        }
         else {
-            return $result;
-        }          
+            $_SESSION['errMsg'] = 'Unauthorized!';
+        }
     }
     public static function addList(Lists $newList){
         $userId = $newList->getUserId();
-        $result = $newList->addList($newList);
-        if($result === false) {
-            echo 'Error in query';
-            return;
-        } 
+        if(AuthController::isLoggedIn() && $_SESSION['userId'] == $userId) {
+            
+            $result = $newList->addList($newList);
+            if($result === false) {
+                echo 'Error in query';
+                return false;
+            } 
+            else {
+                header("Location: ../views/client/lists.php?id=$userId");
+                return $result;
+            } 
+        }
         else {
-            header("Location: ../views/client/lists.php?id=<?php echo htmlspecialchars($userId); ?>");
-            return $result;
-        } 
+            $_SESSION['errMsg'] = 'Unauthorized!';
+        }
     }
     public static function deleteList($listId){
-        $list = new Lists();
-        $result = $list->deleteList($listId);
-        if($result === false) {
-            echo 'Error in query';
-            return;
+        if(AuthController::isLoggedIn()) {
+            $list = new Lists();
+            $result = $list->deleteList($listId);
+            if($result === false) {
+                echo 'Error in query';
+                return;
+            } 
+            else {
+                return $result;
+            }        
         } 
         else {
-            return $result;
-        }        
-
+            $_SESSION['errMsg'] = 'Unauthorized!';
+        }
     }
     public static function editlist(Lists $list){
         $userId = $list->getUserId();
-        $result = $list->editList($list);
-        if($result === false) {
-            echo 'Error in query';
-            return false;
+        if(AuthController::isLoggedIn() && $_SESSION['userId'] == $userId) {
+            $result = $list->editList($list);
+            if($result === false) {
+                echo 'Error in query';
+                return false;
+            } 
+            else {
+                header("Location: lists.php?userId=$userId");           
+                return true;
+            } 
         } 
         else {
-            header("Location: lists.php?userId=" . htmlspecialchars($userId));           
-            return true;
-        } 
+            $_SESSION['errMsg'] = 'Unauthorized!';
+        }
     }
 
     public static function fetchListArticles($listId){
@@ -60,27 +82,32 @@ class ListsController{
         $result = $list->getListArticles($listId);
         if($result === false) {
             echo 'Error in query';
-            return;
+            return [];
         } 
         else if(count($result) === 0) {
-            header('location: 404.php');
+            return [];
         }
         else {
             return $result;
         } 
     }
 
-    public static function removeArticleFromList( $listId,$articleId){
+    public static function removeArticleFromList($listId, $articleId){
 
-        $li = new Lists();
-        $result = $li->removeArticleFromList($listId, $articleId);
-        if($result === false) {
-            echo 'Error in query';
-            return;
-        } 
+        if(AuthController::isLoggedIn()) {
+            $li = new Lists();
+            $result = $li->removeArticleFromList($listId, $articleId);
+            if($result === false) {
+                echo 'Error in query';
+                return;
+            } 
+            else {
+                return $result;
+            } 
+        }
         else {
-            return $result;
-        } 
+            $_SESSION['errMsg'] = 'Unauthorized!';
+        }
 
     }
 
