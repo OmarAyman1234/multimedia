037c63fb0b8779178c3338b0c161cf3b276dbe19
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-08 15:50:45 +0300

    using __DIR__ in require_once

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,12 +1,8 @@
 <?php
-// require_once('./models/client.php');
-require_once '../../models/client.php';
-require_once '../../models/guest.php';
-require_once('../../controllers/DBController.php');
+require_once __DIR__ . '/DBController.php';
 
 class AuthController
 {
-    // public $db;
 
     public static function isAdmin() {
         if(session_status() === PHP_SESSION_NONE)
@@ -51,86 +47,78 @@ class AuthController
 
     //login at RegisteredUser Model, register I don't know yet.
 
-    public function login(Client $user)
-    {
-    $this->db = new DBcontroller();
+    // public function login(Client $user)
+    // {
+    // $this->db = new DBcontroller();
 
-    if ($this->db->openConnection()) {
+    // if ($this->db->openConnection()) {
     
-        $query = "SELECT * FROM registeredusers WHERE username='" . $user->getUsername() . "' AND password='" . $user->getPassword() . "'";
+    //     $query = "SELECT * FROM registeredusers WHERE username='" . $user->getUsername() . "' AND password='" . $user->getPassword() . "'";
 
-        $result = $this->db->select($query);
+    //     $result = $this->db->select($query);
 
-        if ($result === false) {
-            echo "Error in query.";
-            return false;
-        }
+    //     if ($result === false) {
+    //         echo "Error in query.";
+    //         return false;
+    //     }
 
-        if (count($result) == 0) {
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            $_SESSION["errmsg"] = "Wrong username or password.";
-            return false;
-        }
+    //     if (count($result) == 0) {
+    //         if (session_status() === PHP_SESSION_NONE) {
+    //             session_start();
+    //         }
+    //         $_SESSION["errmsg"] = "Wrong username or password.";
+    //         return false;
+    //     }
 
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
+    //     if (session_status() === PHP_SESSION_NONE) {
+    //         session_start();
+    //     }
 
-        $_SESSION["userId"] = $result[0]["id"];
-        $_SESSION["username"] = $result[0]["username"];
-        $_SESSION['roleId'] = $result[0]['roleId'];
+    //     $_SESSION["userId"] = $result[0]["id"];
+    //     $_SESSION["username"] = $result[0]["username"];
+    //     $_SESSION['roleId'] = $result[0]['roleId'];
         
-        if (isset($_SESSION["roleId"])) {
-            if ($_SESSION["roleId"] == 1) {
-                $_SESSION['roleName'] = 'Admin';
-            } elseif ($_SESSION["roleId"] == 2) {
-              $_SESSION['roleName'] ='Editor';
-            } else {
-            $_SESSION['roleName']='Client';
-            }
-        }
-        return true;
-    } else {
-        echo "Error in database connection.";
-        return false;
-    }
-    }
-    public static function registerController(Guest $guest){
-        $result = $guest->register($guest);
-        if ($result != false){
-            if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-            $_SESSION["userId"] = $result;
-            $_SESSION["username"] = $guest->getUsername();
-            $_SESSION['roleId'] = 3;
-            $_SESSION['roleName']='Client';
-            header('location: ../views/client/index.php');
-        }
-        else{
-            header('location: ../Shared/404.php');
-        }
-        
-        return true;
-        }
-    }
-
-    public static function addList($newList){
-        $userId = $newList->getUserId();
-        $result = $newList->addList($newList);
-        if($result === false) {
-            echo 'Error in query';
-            return;
-        } 
-        else {
-            header("Location: ../views/client/lists.php?id=<?php echo htmlspecialchars($userId); ?>");
-            return $result;
-        } 
-
-    }
-
-
+    //     if (isset($_SESSION["roleId"])) {
+    //         if ($_SESSION["roleId"] == 1) {
+    //             $_SESSION['roleName'] = 'Admin';
+    //         } elseif ($_SESSION["roleId"] == 2) {
+    //           $_SESSION['roleName'] ='Editor';
+    //         } else {
+    //         $_SESSION['roleName']='Client';
+    //         }
+    //     }
+    //     return true;
+    // } else {
+    //     echo "Error in database connection.";
+    //     return false;
+    // }
+    // }
+    // public function register(Client $user){
+    //     $this->db = new DBController;
+    //     $uUsername = $user->getUsername();
+    //     $uPassword = $user->getPassword();
+    //     $uEmail = $user->getEmail();
+    //     if($this->db->openConnection()){
+    //         $query = "INSERT INTO registeredusers (username, email, password, roleId) VALUES ('$uUsername', '$uEmail', '$uPassword', 3)"; 
+    //         $result = $this->db->insert($query);
+    //         $query1 = "INSERT INTO lists (name, userId) VALUES ('Bookmark', '$result')";
+    //         $result1 = $this->db->insert($query1);
+    //         if ($result != false){
+    //             if (session_status() === PHP_SESSION_NONE) {
+    //             session_start();
+    //             $_SESSION["userId"] = $result;
+    //             $_SESSION["username"] = $uUsername;
+    //             $_SESSION['roleId'] = 3;
+    //             $_SESSION['roleName']='Client';
+    //         }
+    //         return true;
+    //         }
+    //     }
+    //     else{
+    //         echo "Error in database connection";
+    //         return false;
+    //     }
+    // }
 
 }
 ?>
