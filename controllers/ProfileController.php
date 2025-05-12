3c16565acd6dc905e1e69f1bacd804feb163bac5
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-12 22:52:58 +0300

    alert for profile

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -6,25 +6,28 @@ require_once '../../models/registereduser.php';
 class ProfileController {
     public static function fetchProfileData($userId) {
         if(AuthController::isLoggedIn()) {
-            $result = RegisteredUser::fetchProfileData($userId);
-            return $result;
+            return RegisteredUser::fetchProfileData($userId);
         }
         else {
-            // session started when checking if the user is logged in or not
-            $_SESSION['errMsg'] = 'Unauthorized!';
+            Alert::setAlert('danger', 'Unauthorized!');
+            header('location: ../../views/auth/login.php');
+            exit;
         }
     }
 
     public static function updateEmail($newEmail) {
         if(AuthController::isLoggedIn()) {
-            // session started when checking if the user is logged in or not
             RegisteredUser::updateEmail($_SESSION['userId'], $newEmail);
-            return true;
+
+            Alert::setAlert('success', 'Email updated!');
+
+            header("location: ../../views/Shared/profile.php?id=" . $_SESSION['userId']);
+            exit;
         }
         else {
-            // session started when checking if the user is logged in or not
-            $_SESSION['errMsg'] = 'Unauthorized!';
-            return false;
+            Alert::setAlert('danger', 'Unauthorized!');
+            header('location: ../../views/auth/login.php');
+            exit;
         }
     }
     
@@ -32,93 +35,33 @@ class ProfileController {
         if(AuthController::isLoggedIn()) {
             $hashedNewPass = password_hash($newPass, PASSWORD_BCRYPT);
             
-            // session started when checking if the user is logged in or not
             RegisteredUser::updatePassword($_SESSION['userId'], $hashedNewPass);
-            return true;
+
+            Alert::setAlert('success', 'Password changed!');
+
+            header("location: ../../views/Shared/profile.php?id=" . $_SESSION['userId']);
+            exit;
         }
         else {
-            // session started when checking if the user is logged in or not
-            $_SESSION['errMsg'] = 'Unauthorized!';
-            return false;
+            Alert::setAlert('danger', 'Unauthorized!');
+            header('location: ../../views/auth/login.php');
+            exit;
         }
     }
     public static function updateProfilePicture($newProfilePicture) {
         if(AuthController::isLoggedIn()) {
-            // session started when checking if the user is logged in or not
             RegisteredUser::updateProfilePicture($_SESSION['userId'], $newProfilePicture);
-            return true;
+
+            Alert::setAlert('success', 'Profile picture changed!');
+
+            header("location: ../../views/Shared/profile.php?id=" . $_SESSION['userId']);
+            exit;
         }
         else {
-            // session started when checking if the user is logged in or not
-            $_SESSION['errMsg'] = 'Unauthorized!';
-            return false;
+            Alert::setAlert('danger', 'Unauthorized!');
+            header('location: ../../views/auth/login.php');
+            exit;
         }
     }
 }
-//$db= new DBController;
-//     public static function changePassword($newPass,$userId) {
-//         $db = new DBController();
-//         if($db->openConnection()) {
-//             $user = $db->getConnection()->prepare("UPDATE registeredusers SET password = ? WHERE id = ?");
-//             $user->bind_param("si", $newPass, $userId);
-//             $user->execute();
-//         }
-//  }
- 
-
-//     public static function changeEmail($newEmail,$userId) {
-//         $db = new DBController();
-//         if($db->openConnection()) {
-//             $user = $db->getConnection()->prepare("UPDATE registeredusers SET email = ? WHERE id = ?");
-//             $user->bind_param("si", $newEmail, $userId);
-//             $user->execute();
-//         }
-//     }
-//   }
-//  $db = new DBController();
-
-//  if ($db->openConnection()) {
-//     $userId = $_SESSION['userId'];
-
-//     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//         if (isset($_POST['field']) && isset($_POST['value'])) {
-//             $field = $_POST['field'];
-//             $newValue = htmlspecialchars($_POST['value']);
-        
-
-          
-//             $allowedFields = ['email', 'password','joinDate'];
-//             if (in_array($field, $allowedFields)) {
-
-//                 if ($field === 'password') {
-//                     $newValue = password_hash($newValue, PASSWORD_DEFAULT);
-//                 }
-
-          
-//                 $user = $dbcontroller->connection->prepare("UPDATE registeredusers SET $field = ? WHERE id = ?");
-//                 $user->bind_param("si", $newValue, $userId);
-//                 $user->execute();
-//             }
-//         }
-//     }
-
-   
-
-//     if ($_SESSION["roleId"] == 1) {
-//         $_SESSION['roleName'] = 'Admin';
-//     } elseif ($_SESSION["roleId"] == 2) {
-//         $_SESSION['roleName'] = 'Editor';
-//     } else {
-//         $_SESSION['roleName'] = 'Client';
-//     }
-
-//     if ($result && count($result) > 0) {
-//         $user = $result[0];
-
-
-
-//     }
-// }
-
-
 ?>
