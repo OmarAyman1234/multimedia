f017654431f8962b3604d8dcfcf8350c6f26e77c
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-12 21:03:48 +0300

    added alerts in report controller

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -2,6 +2,7 @@
 require_once '../../models/report.php';
 require_once '../../models/reportreasons.php';
 require_once '../../controllers/AuthController.php';
+require_once '../../views/utils/alert.php';
 
 class ReportController {
   public static function getReportReasons() {
@@ -9,8 +10,12 @@ class ReportController {
   }
   
   public static function sendReportToAdmin(Report $r) {
-    if(AuthController::isLoggedIn()) {
+    if(AuthController::isLoggedIn() && !AuthController::isAdmin()) {
       Report::saveReport($r);
+      Alert::setAlert('success', 'Report sent to admins and they will review it!');
+    }
+    else {
+      Alert::setAlert('danger', 'Admins cannot report or you have to log in');
     }
   }
 
@@ -19,6 +24,8 @@ class ReportController {
       return Report::getReports();
     }
     else {
+      Alert::setAlert('danger', 'You do not have access to this page');
+      
       header('location: ../../views/auth/login.php');
       exit;
     }
@@ -27,8 +34,10 @@ class ReportController {
   public static function dismissReport($reportId) {
     if(AuthController::isAdmin()) {
       Report::dismissReport($reportId);
+      Alert::setAlert('danger', "Report #$reportId dismissed");
     }
     else {
+      Alert::setAlert('danger', 'You do not have access to this page');
       header('location: ../../views/auth/login.php');
       exit;
     }
