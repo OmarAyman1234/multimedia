44bb2ed35141983c5c032cf0b037dfd0922260d2
OmarAyman1234 <omarayman9124@gmail.com>
2025-05-12 20:46:03 +0300

    removed exclamation mark

--- views/utils/alert.php
+++ views/utils/alert.php
@@ -9,7 +9,7 @@ class Alert {
       echo '
         <div style="position: fixed; top: 20px; right: 20px; width: 25rem; z-index: 1050;">
           <div class="alert alert-' . htmlspecialchars($_SESSION['alert']['type']) . ' alert-dismissible fade show" role="alert">
-            <i class="fa fa-exclamation-circle me-2"></i> ' . htmlspecialchars($_SESSION['alert']['message']) . '
+            ' . htmlspecialchars($_SESSION['alert']['message']) . '
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
         </div>
